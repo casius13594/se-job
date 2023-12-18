09598e4a63294b7205e692b1fa0ce2f3e72aafd9
Duc7111 <phantrungduc2003@gmail.com>
2023-12-18 10:38:07 +0700

    login test

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -2,8 +2,7 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export const createClient = () => {
-  const cookieStore = cookies()
+export const createClient = (cookieStore: ReturnType<typeof cookies> = cookies()) => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
