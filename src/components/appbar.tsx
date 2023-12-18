042a02a98860033a431e6ea3b5a4f85dc6c1f961
casius13594 <sswvn30@gmail.com>
2023-12-18 11:06:28 +0700

    change to nav bar

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
 import React from "react";
 import Image from 'next/image';
-import Button from "./appbarbuttons";
-
+import { usePathname } from "next/navigation";
+import { BiLogIn} from 'react-icons/bi';
+import './component.css'
+import classnames from 'classnames';
+import { Button, ThemePanel } from "@radix-ui/themes"
+import { IoIosNotifications, IoMdHome, IoMdDocument   } from "react-icons/io";
 const AppBar = () => {
-
+    const currentPath = usePathname();
+    console.log(currentPath);
+    const links = [
+    // put property in here.
+    { href: '/auth/login', icon: <IoMdHome />,name: 'Home' },
+    { href: '/auth/login', icon: <IoMdDocument  />,name: 'Documents' },
+    { href: '/auth/login', icon: <IoIosNotifications />,name: 'Notifications' },
+    ];
     return (
-
             <header className="w-full absolute z-10">
-              <nav className="max-w-[1440p] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+              <nav className="max-w-[1440p] mx-auto flex justify-between items-center px-6 py-2">
                 <Link href="/" className="flex justify-center items-center">
                   <Image
                     src="/logo.svg"
@@ -19,20 +29,25 @@ const AppBar = () => {
                   />
                 </Link>
         
-                <div className="flex flex-row space-x-3">
-                  <Button
-                    text="Home"
-                    iconSrc="/home.svg"
-                  />
-                  <Button
-                    text="Job applied"
-                    iconSrc="/home.svg"
-                  />
-                  <Button
-                    text="Home"
-                    iconSrc="/home.svg"
-                  />
-                </div>
+                <ul className='flex space-x-6'>
+                {links.map((link) => (
+                  <Link
+                    key={link.href}
+                    className={classnames({
+                      'text-zinc-900': link.href === currentPath,
+                      'text-zinc-500': link.href !== currentPath,
+                      'hover:text-zinc-800 transition-colors': true,
+                      'flex flex-col items-center text-center': true, // Added flex styles for centering
+                    })}
+                    href={link.href}
+                  >
+                    <Button className="circle-button">
+                      {link.icon} {/* Use the icon React component here */}
+                    </Button>
+                    <p className="link-name font-sans text-sm">{link.name}</p>
+                  </Link>
+                ))}
+              </ul>
               </nav>
             </header>
           );
