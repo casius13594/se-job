08093cb59883079cc6e7ba9ebde84b30da15f103
casius13594 <sswvn30@gmail.com>
2023-12-19 13:53:12 +0700

    selection in form

--- src/app/(content)/auth/profileinput/CreateForm.tsx
+++ src/app/(content)/auth/profileinput/CreateForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react'
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Profile.css'; 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Areaselector from '@/components/Areaselector';
@@ -30,11 +30,23 @@ const CreateForm = () => {
     const[intro,setIntro] = useState('')
     const[isLoading, setIsLoading] = useState(false)
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.id]: e.target.value
-        }))
+    console.log(formData)
+    const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { id, value } = e.target;
+    
+        if (id === 'name') {
+            const formattedName = value.toLowerCase().trim().replace(/\s+/g, '-');
+            setFormData((prevState) => ({
+                ...prevState,
+                [id]: value,
+                url: `jelp/company/${formattedName}`
+            }));
+        } else {
+            setFormData((prevState) => ({
+                ...prevState,
+                [id]: value
+            }));
+        }
     }
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
@@ -103,7 +115,7 @@ const CreateForm = () => {
                 className='p-1 border rounded-md w-full'
                 ></input>
             </div>
-            <div className='w-full flex flex-col items-start mb-4'>
+            {/*<div className='w-full flex flex-col items-start mb-4'>
             <label htmlFor='location' className='font-bold'>
                 Location <span className='text-red'>*</span>
             </label>
@@ -116,7 +128,7 @@ const CreateForm = () => {
                 value={location}
                 className='p-1 border rounded-md w-full'
             ></input>
-            </div>
+  </div>*/}
 
             <div className='w-full flex flex-col items-start mb-4'>
             <label htmlFor='phone' className='font-bold'>
@@ -163,41 +175,52 @@ const CreateForm = () => {
             ></input>
             </div>
             <div className='w-full flex flex-col items-start mb-4'>
-            <label htmlFor='size' className='font-bold'>
-                Size <span className='text-red'>*</span>
-            </label>
-            <input
-                required
-                type='text'
-                autoComplete='off'
-                id = 'size'
-                onChange={onChange}
-                value={size}
-                className='p-1 border rounded-md w-full'
-            ></input>
+                <label htmlFor='size' className='font-bold'>
+                    Size <span className='text-red'>*</span>
+                </label>
+                <select
+                    required
+                    id='size'
+                    onChange={onChange}
+                    value={size}
+                    className='p-1 border rounded-md w-full'
+                >
+                    <option value='' disabled>Select a Size Range</option>
+                    <option value='0-10'>0 - 10</option>
+                    <option value='10-50'>10 - 50</option>
+                    <option value='50-100'>50 - 100</option>
+                    <option value='100-500'>100 - 500</option>
+                    <option value='500-1000'>500 - 1000</option>
+                    <option value='1000-10000'>1000 - 10000</option>
+                    <option value='10000++'>10000 ++</option>
+                </select>
             </div>
 
             <div className='w-full flex flex-col items-start mb-4'>
-            <label htmlFor='type' className='font-bold'>
-                Type <span className='text-red'>*</span>
-            </label>
-            <input
-                required
-                type='text'
-                autoComplete='off'
-                id = 'type'
-                onChange={onChange}
-                value={type}
-                className='p-1 border rounded-md w-full'
-            ></input>
-            </div>
+                    <label htmlFor='type' className='font-bold'>
+                        Type <span className='text-red'>*</span>
+                    </label>
+                    <select
+                        required
+                        id='type'
+                        onChange={onChange}
+                        value={type}
+                        className='p-1 border rounded-md w-full'
+                    >
+                        <option value='' disabled>Select a Type</option>
+                        <option value='Public company'>Public company</option>
+                        <option value='Government agency'>Government agency</option>
+                        <option value='Privately held'>Privately held</option>
+                        <option value='Nonprofit'>Nonprofit</option>
+                    </select>
+                </div>
             </div>
             <div className='right-column'>
             <div className='w-full flex flex-col items-start mb-4'>
             <label htmlFor='location' className='font-bold'>
                 Location <span className='text-red'>*</span>
             </label>
-                    <Areaselector></Areaselector>
+                    <Areaselector ></Areaselector>
                     </div>
             <div className='w-full flex flex-col items-start mb-4'>
             <label htmlFor='intro' className='font-bold'>
