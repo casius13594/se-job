6b6d2ad9d4f0d9b3cc9400f2419e141001564f9d
casius13594 <sswvn30@gmail.com>
2023-12-20 19:27:01 +0700

    reformat register page

--- src/app/(content)/auth/register/registerform.tsx
+++ src/app/(content)/auth/register/registerform.tsx
@@ -10,8 +10,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useSearchParams,useRouter } from 'next/navigation';
 import { link } from 'fs';
-
-
+import { MdOutlineAccountCircle } from "react-icons/md";
 const userSchema = z
 .object({
     email: z.string().min(1,'Email is required').email('Invalid email'),
@@ -92,19 +91,19 @@ export default function Register() {
   return (
     <main className="flex min-h-screen  items-center justify-between">
       <div className='flex min-h-screen flex-col min-w-full items-center '>
-        <div className="relative top-0 left-0 flex border-b border-black w-full items-end lg:static lg:h-auto lg:bg-none ">
+        <div className="relative top-0 left-0 flex border-b w-full items-end lg:static lg:h-auto lg:bg-none ">
             <Link href='/'>
               <Image
               src="/logo.svg"
               alt="Jelp logo"
-              width="125"
-              height="125"
+              width="100"
+              height="100"
               className="object-contain mx-7 mt-7 mb-2"
               />
             </Link>
         </div>
         
-        <div className='relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2x '>
+        <div className='relative flex flex-col bg-white shadow-2xl rounded-2x '>
           <div onSubmit= {handleSubmit(onSubmit)} className='flex flex-col justify-center p-8'>
             <div className='relative  flex pr-2 pl-2 items-center justify-center border-b border-black'>
               <button
@@ -117,28 +116,36 @@ export default function Register() {
             </div> 
             
             <form onSubmit= {handleSubmit(onSubmit)} className='flex flex-col justify-center'>
-              <div className='p-1 mt-10 mb-2 items-center bg-[#D9D9D9] rounded-2xl'>
-                <img src='/iconPassword.svg' className = " inline ml-2 mr-2 pr-2 border-r border-separate"></img>
+            <div className="flex items-center mb-5 mt-5">
+                <div className="absolute m-5 flex">
+                  <Image src="/2User.svg" alt="Email" width={20} height={20} />
+                  <div className="h-10 bg-black w-px ml-5"></div>
+                </div>
                 <input 
                   {...register("email")}
                   type="email" 
-                  className='p-2 focus:outline-none focus:bg-[#D9D9D9] focus:text-gray-900 bg-[#D9D9D9] font-semibold'
+                  className="w-full pl-20 py-4 bg-[#D9D9D9] text-black text-opacity-50 lg:text-base md:text-sm sm:text-xs font-dmsans rounded-lg placeholder-black placeholder-opacity-50"
                   name='email'
                   id='email'
-                  placeholder='Username or gmail'                  autoComplete='off'
+                  placeholder='Gmail'                 
                   >
                 </input>
               </div>
+
+              
               {errors.email && (
                   <p className='text-red font-semibold'>{`${errors.email.message}`}</p>
                 )}
 
-              <div className='p-1 my-2 items-center bg-[#D9D9D9] rounded-2xl'>
-                <img src='/iconPassword.svg' className = " inline ml-2 mr-2 pr-2 border-r border-separate"></img>
+                <div className="flex items-center mb-5">
+                                <div className="absolute m-5 flex">
+                                  <Image src='/iconPassword.svg' alt="Email" width={20} height={20} />
+                                  <div className="h-10 bg-black w-px ml-5"></div>
+                                </div>
                 <input 
                   {...register("password")}
                   type="password" 
-                  className='p-2 focus:outline-none focus:bg-[#D9D9D9] focus:text-gray-900 bg-[#D9D9D9] font-semibold'
+                  className="w-full pl-20 py-4 bg-[#D9D9D9] text-black text-opacity-50 lg:text-base md:text-sm sm:text-xs font-dmsans rounded-lg placeholder-black placeholder-opacity-50"
                   name='password'
                   id='password'
                   placeholder='Password'
@@ -150,12 +157,15 @@ export default function Register() {
                   <p className='text-red font-semibold'>{`${errors.password.message}`}</p>
                 )}
               
-              <div className='p-1 my-2 items-center bg-[#D9D9D9] rounded-2xl'>
-                <img src='/iconPassword.svg' className = " inline ml-2 mr-2 pr-2 border-r border-separate"></img>
+              <div className="flex items-center mb-5">
+                                <div className="absolute m-5 flex">
+                                  <Image src='/iconPassword.svg' alt="Email" width={20} height={20} />
+                                  <div className="h-10 bg-black w-px ml-5"></div>
+                                </div>
                 <input 
                   {...register("confirm")}
                   type="password" 
-                  className='p-2 focus:outline-none focus:bg-[#D9D9D9] focus:text-gray-900 bg-[#D9D9D9] font-semibold'
+                  className="w-full pl-20 py-4 bg-[#D9D9D9] text-black text-opacity-50 lg:text-base md:text-sm sm:text-xs font-dmsans rounded-lg placeholder-black placeholder-opacity-50"
                   name='confirm'
                   id='confirm'
                   placeholder='Re-enter'
@@ -169,13 +179,13 @@ export default function Register() {
               
               <button
                 type="submit"
-                className='my-4 p-2 text-center rounded-2xl text-white font-semibold focus:outline-none focus:bg-[#000000] items-center justify-center bg-[#000000] opacity-20 '>
+                className='my-4 p-2 text-center rounded-2xl text-white font-semibold focus:outline-none bg-black opacity-20 hover:bg-black hover:opacity-100'>
                   Sign in
               </button>
 
               <div className='text-center text-black font-bold text-xs'>
                 Already have an account? 
-                <span className="font-semibold text-[#505050] text-opacity-70"> Log in now</span>
+                <span className="font-semibold text-[#505050] text-opacity-70 hover:text-opacity-100"> <Link href='./login' >Log in now</Link></span>
               </div>
 
               
