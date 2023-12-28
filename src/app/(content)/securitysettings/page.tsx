fd3cc4b3076b985f53ab5d32985b991ce9d4c0bf
TruongTho3345 <100506191+TruongTho3345@users.noreply.github.com>
2023-12-29 01:34:05 +0700

    Optimize code in security settings page

--- src/app/(content)/securitysettings/page.tsx
+++ src/app/(content)/securitysettings/page.tsx
@@ -1,84 +1,15 @@
 "use client";
 
 import { UserInfo } from "@/components/userinfo";
-import React from "react";
 import AppBar from "@/components/appbar";
 import { dm_sans } from "@/components/fonts";
-import { getUser } from "@/components/controller";
-import Areaselector from "@/components/Areaselector";
-import { ICity, ICountry, IState } from "country-state-city";
-import { Country } from "country-state-city";
-import Modal from "react-modal";
 
 export default function BasicInfo() {
-  const [modified, setModified] = React.useState(false);
-  const [reload, setReload] = React.useState(false);
-  const [user, setUser] = React.useState<any>(null);
-
-  let countryData = Country.getAllCountries();
-  const [country, setCountry] = React.useState<ICountry>(countryData[0]);
-  const [state, setState] = React.useState<IState | undefined>(undefined);
-  const [city, setCity] = React.useState<ICity | undefined>(undefined);
-  const [locationString, setLocationString] = React.useState<string>("");
-
-  const [selectLocation, setSelectLocation] = React.useState(false);
-
-  React.useEffect(() => {
-    const fetchProfile = async () => {
-      const userObject = await getUser();
-      if (userObject) {
-        const { data: user, isEmployee: isEmployee } = userObject;
-        if (user) {
-          setUser(user);
-          setLocationString(user.location);
-        }
-      }
-    };
-    fetchProfile();
-  }, [reload]);
-
   return (
     <main
       className={`flex flex-col w-[100vw] h-[100vh] ${dm_sans.className} pt-[2vh] overflow-hidden`}
     >
       <AppBar />
-      <Modal
-        isOpen={selectLocation}
-        onRequestClose={() => setSelectLocation(false)}
-      >
-        <div className="flex flex-col items-center justify-center">
-          <Areaselector
-            city={city}
-            setCity={setCity}
-            country={country}
-            setCountry={setCountry}
-            state={state}
-            setState={setState}
-            showDistrict={true}
-          />
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-            onClick={() => {
-              setSelectLocation(false);
-              setLocationString(
-                `${city?.name || ""} ${", " + state?.name || ""} ${
-                  ", " + country?.name || ""
-                }`
-              );
-            }}
-          >
-            Apply
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-            onClick={() => {
-              setSelectLocation(false);
-            }}
-          >
-            Cancel
-          </button>
-        </div>
-      </Modal>
       <UserInfo
         isBasic={false}
         content={
